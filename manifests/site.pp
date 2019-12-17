ccf67051079f6b22606c9374978416afb2685109
dgowan777 <58914160+dgowan777@users.noreply.github.com>
2019-12-17 15:21:37 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,9 @@
 node default {
   file {'/root/README':
     ensure  => file,
-    content => 'Please read this file in it's entirty before proceeding...\n',
+    content => 'Please read this file in it\'s entirty before proceeding...
+    Thank you.
+    ',
     owner   => 'root',
   }
 }
